e398d948cc2a16d8d0c619614695acc30d3d9ba1
automorphis <mlanetheta@gmail.com>
2022-03-02 13:25:41 -0500

    woops

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from itertools import product
 from math import comb
 
 """ `N` is the size of the underlying set of the topology. """
@@ -24,16 +25,34 @@ The following code creates a list `inventories` of all possible naive inventorie
 ranges = []
 
 for k in range(N):
-    ranges.append( range(comb(N,k) + 1) )
+    ranges.append( range(comb(N, k + 1) + 1) )
 
 inventories = []
 
-for rng in ranges:
+"""
+The idiom `product(*tuple(ranges))` found below is a bit tricky to understand but it is extremely useful.
 
-    inv = []
-    for x in rng:
-        inv.append(x)
+Recall that the function `product` is a Python standard library function. It takes the Cartesian
+product of an arbitrary number of iterables. You can find the docs for `product` here:
+https://docs.python.org/3/library/itertools.html#itertools.product
 
+A naive inventory is an element of
+`product(ranges[0], ranges[1], ranges[2], ..., ranges[N-1])`
+
+However, we cannot use the `...` syntax in this way in Python. Instead, we must use the unary 
+"unpacking" operator, denoted by `*`. This operator takes a tuple and turns each element of the 
+tuple into an argument of a function. 
+
+So for example, you could do
+`comb(*(5,3))`
+which gives you the same number as `comb(5,3)` does. The operator `*` is useless in this context.
+However, `*` is very useful when the function can take a variable number of arguments, like
+`product` does. 
+
+So `product(*tuple(ranges))` takes the list `ranges`, converts it to a tuple, then "unpacks" it
+for the variable-parameter function `product`.  
+"""
+for inv in product(*tuple(ranges)):
     inventories.append(inv)
 
 
